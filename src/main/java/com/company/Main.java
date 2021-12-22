fdeead349b0e39b24b35a4e8ba4a90bda19a07e5
SangHun <sanghun1984@gmail.com>
2021-12-22 14:03:35 +0900

    add classloader

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,20 @@
 package com.company;
 
+import java.awt.print.Book;
+
 public class Main {
+
+    static String name;
+
+    static {
+        name = "sanghun";
+    }
 
     public static void main(String[] args) {
 	// write your code here
         ClassLoader classLoader = Main.class.getClassLoader();
+        System.out.println(classLoader);
+        System.out.println(classLoader.getParent());
+        System.out.println(classLoader.getParent().getParent());
     }
 }
