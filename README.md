6777aa501e66c8c5b8b948a58895dd24336c6919
SangHun <sanghun1984@gmail.com>
2021-12-27 14:37:25 +0900

    add javaagent

---
+++ README.md
@@ -0,0 +1,10 @@
+## run with javaagent
+```shell
+java -javaagent:/Users/computer/sources/study/masulsa-agent/app/build/libs/app.jar -jar thejava-1.0-SNAPSHOT.jar
+```
+
+## without javaagent
+
+```shell
+java -jar thejava-1.0-SNAPSHOT.jar
+```
--- src/main/java/kr/co/sanghun/study/Masulsa.java
+++ src/main/java/kr/co/sanghun/study/Masulsa.java
@@ -1,7 +1,9 @@
 package kr.co.sanghun.study;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,14 +14,18 @@ public class Masulsa {
 
     public static void main(String[] args) {
 
-        // ByteBuddy 실행시 byte code 변경됨
-        try {
-            new ByteBuddy().redefine(Moja.class)
-                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
-                    .make().saveIn(new File("/Users/computer/sources/study/thejava/target/classes"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        ClassLoader classLoader = Masulsa.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
+//
+//        // ByteBuddy 실행시 byte code 변경됨
+//        try {
+//            new ByteBuddy().redefine(typePool.describe("kr.co.sanghun.study.Moja").resolve(),
+//                    ClassFileLocator.ForClassLoader.of(classLoader))
+//                    .method(named("pullOut")).intercept(FixedValue.value("Rabbit!"))
+//                    .make().saveIn(new File("/Users/computer/sources/study/thejava/target/classes"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println(new Moja().pullOut());
     }
